a066c351cb1476a1dfc00f76b4aeb5867abda8cd
javeria2108 <javeriazh@gmail.com>
2024-07-27 23:32:46 +0500

    liveblocks auth

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,23 +1,9 @@
-import { Editor } from "@/components/editor/Editor";
-import Header from "@/components/Header";
-import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const Document = () => {
   return (
     <div>
-      <Header>
-        <div className="flex w-fit items-center justify-center gap-2">
-          <p className="document-title">This is title</p>
-        </div>
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </Header>
-      <Editor />
+      
     </div>
   );
 };
---
+++ lib/liveblocks.ts
@@ -0,0 +1,6 @@
+import { Liveblocks } from "@liveblocks/node";
+
+export const liveblocks = new Liveblocks({
+    secret: process.env.LIVEBLOCKS_SECRET_KEY as string,
+  });
+  
