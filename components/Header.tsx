0342e5ae545eaa3c324a3cbf8876281196e24859
javeria2108 <javeriazh@gmail.com>
2024-07-28 00:09:00 +0500

    add 'addDocumentBtn' button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,28 +1,27 @@
+import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
-import React, { Children } from 'react'
 
-const Header = ({children}: HeaderProps) => {
+const Header = ({ children, className }: HeaderProps) => {
   return (
-    <div className='header'>
-        <Link href='/' className='md:flex-1'>
-        <Image
-        src='/assets/icons/logo.svg'
-        alt='logo with name'
-        width={120}
-        height={32}
-        className='hidden md:block'
+    <div className={cn("header", className)}>
+      <Link href='/' className="md:flex-1">
+        <Image 
+          src="/assets/icons/logo.svg"
+          alt="Logo with name"
+          width={120}
+          height={32}
+          className="hidden md:block"
         />
-         <Image
-        src='/assets/icons/logo-icon.svg'
-        alt='logo'
-        width={32}
-        height={32}
-        className='mr-2 md:hidden'
+        <Image 
+          src="/assets/icons/logo-icon.svg"
+          alt="Logo"
+          width={32}
+          height={32}
+          className="mr-2 md:hidden"
         />
-        </Link>
-        {children}
-
+      </Link>
+      {children}
     </div>
   )
 }
