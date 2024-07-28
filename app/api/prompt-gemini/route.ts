f23050186a1f708d165dad2b9d3aab441b3e7685
Adrian Gallano <adriangallanoengr@gmail.com>
2024-07-28 15:40:58 +0800

    Change pro to flash

--- app/api/prompt-gemini/route.ts
+++ app/api/prompt-gemini/route.ts
@@ -11,7 +11,7 @@ export async function POST(request: NextRequest) {
     const genAI = new GoogleGenerativeAI(apiKey);
 
     const model = genAI.getGenerativeModel({
-        model: "gemini-1.5-pro",
+        model: "gemini-1.5-flash",
     });
 
     const generationConfig = {
